98d37713a4e554ee28be8c092d102bd9a2111217
Seba2997 <valdebenito732@gmail.com>
2025-05-21 18:10:04 -0400

    agregando proyecto api_inscripcion

---
+++ api_inscripcion/api_inscripcion/src/main/java/com/eduplatform/api_inscripcion/ApiInscripcionApplication.java
@@ -0,0 +1,13 @@
+package com.eduplatform.api_inscripcion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInscripcionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiInscripcionApplication.class, args);
+	}
+
+}
---
+++ api_inscripcion/api_inscripcion/src/test/java/com/eduplatform/api_inscripcion/ApiInscripcionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduplatform.api_inscripcion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiInscripcionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
