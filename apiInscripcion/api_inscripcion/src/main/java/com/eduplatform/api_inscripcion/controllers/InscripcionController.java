a1237473d9cba9d38728eeba4e721ebace252b6e
Fabian9124 <fabi.morales@duocuc.cl>
2025-05-21 23:16:31 -0400

    eliminarInscripcion controller

--- apiInscripcion/api_inscripcion/src/main/java/com/eduplatform/api_inscripcion/controllers/InscripcionController.java
+++ apiInscripcion/api_inscripcion/src/main/java/com/eduplatform/api_inscripcion/controllers/InscripcionController.java
@@ -3,6 +3,8 @@ package com.eduplatform.api_inscripcion.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,5 +37,9 @@ public class InscripcionController {
         return inscripcionService.inscribirUsuario(idEstudiante, idCurso);
     }
 
-
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> eliminarInscripcion(@PathVariable int id){
+    inscripcionService.eliminar(id);
+    return ResponseEntity.ok("Inscripcion Eliminada");
+} 
 }
