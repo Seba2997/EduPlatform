aa9507efac63cd93d8f7cb0b40deafe43632d7f5
Fabian9124 <fabi.morales@duocuc.cl>
2025-05-23 23:26:46 -0400

    cambios

--- apiInscripcion/api_inscripcion/src/main/java/com/eduplatform/api_inscripcion/controllers/InscripcionController.java
+++ apiInscripcion/api_inscripcion/src/main/java/com/eduplatform/api_inscripcion/controllers/InscripcionController.java
@@ -1,7 +1,5 @@
 package com.eduplatform.api_inscripcion.controllers;
-
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
