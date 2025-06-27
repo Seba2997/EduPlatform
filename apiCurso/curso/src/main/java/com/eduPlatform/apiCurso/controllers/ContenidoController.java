16bfc5f884917975e689b723a9c47eec03b4873d
Fabian9124 <fabi.morales@duocuc.cl>
2025-06-27 18:08:11 -0400

    Agregar documentacion de Curso y contenido con OpenApi

--- apiCurso/curso/src/main/java/com/eduPlatform/apiCurso/controllers/ContenidoController.java
+++ apiCurso/curso/src/main/java/com/eduPlatform/apiCurso/controllers/ContenidoController.java
@@ -18,6 +18,7 @@ import com.eduPlatform.apiCurso.models.requests.ContenidoCrear;
 import com.eduPlatform.apiCurso.models.requests.ContenidoEditar;
 import com.eduPlatform.apiCurso.services.ContenidoService;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 
 @RestController
@@ -28,27 +29,37 @@ public class ContenidoController {
     private ContenidoService contenidoService;
 
     @GetMapping("/")
+    @Operation(summary = "Traer todos los contenidos",
+            description = "Obtiene todos los contenidos cargados en el sistema.")
     public List<Contenido> traerTodos(){
         return contenidoService.obtenerTodos();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Traer contenidos por id",
+            description = "Obtiene un contenido por su id correspondient.")
     public Contenido traerPorId(@PathVariable int id) {
         return contenidoService.obtenerContenidoPorId(id);
     }
 
     @PostMapping("/{idCurso}")
+    @Operation(summary = "Registrar contenido",
+            description = "Registra un nuevo contenido en el sistema.")
     public Contenido registrarContenido(@Valid @RequestBody ContenidoCrear nuevo, @PathVariable int idCurso) {
         return contenidoService.registrar(nuevo, idCurso);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Modificar contenido",
+            description = "Modifica un nuevo contenido en el sistema.")
     public Contenido modificar(@PathVariable Integer id, @Valid @RequestBody ContenidoEditar body) {
         body.setIdContenido(id); 
         return contenidoService.modificar(body);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Eliminar contenido",
+            description = "Elimina un nuevo contenido en el sistema.")
     public ResponseEntity<String> eliminarContenido(@PathVariable int id) {
         contenidoService.eliminarContenido(id);
         return ResponseEntity.ok("Contenido Eliminado");
--- apiCurso/curso/src/main/java/com/eduPlatform/apiCurso/controllers/CursoController.java
+++ apiCurso/curso/src/main/java/com/eduPlatform/apiCurso/controllers/CursoController.java
@@ -18,6 +18,7 @@ import com.eduPlatform.apiCurso.models.requests.CursoCrear;
 import com.eduPlatform.apiCurso.models.requests.CursoEditar;
 import com.eduPlatform.apiCurso.services.CursoService;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 
 @RestController
@@ -27,33 +28,48 @@ public class CursoController {
     @Autowired
     private CursoService cursoService;
 
+    
     @GetMapping("/")
+    @Operation(summary = "Obtiene todos los cursos",
+            description = "Devuelve una lista de todos los cursos registrados en el sistema.")
     public List<Curso> traerTodos(){
         return cursoService.obtenerTodos();
     }
 
+
     @GetMapping("/{id}")
+    @Operation(summary = "Obtiene todos los cursos por id",
+            description = "Devuelve una lista de todos los cursos registrados en el sistema por id.")
     public Curso traerPorId(@PathVariable int id) {
         return cursoService.obtenerCursoPorId(id);
     }
 
+
     @GetMapping("/activos")
+    @Operation(summary = "Obtiene todos los cursos activos",
+            description = "Devuelve una lista de todos los cursos activos registrados en el sistema.")
     public List<Curso> traerActivos(){
         return cursoService.obtenerActivos();
     }
     
     @PostMapping("/{idUsuario}")
+    @Operation(summary = "Crea un nuevo curso",
+            description = "Crea un cursos en el sistema.")
     public Curso crearCurso(@Valid @RequestBody CursoCrear nuevo, @PathVariable int idUsuario){
         return cursoService.registrar(nuevo, idUsuario);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Modifica un nuevo curso",
+            description = "Modifica un cursos en el sistema.")
     public Curso modificar(@PathVariable Integer id, @Valid @RequestBody CursoEditar body) {
     body.setId(id); 
     return cursoService.modificar(body);
     }
     
     @GetMapping("/buscar")
+    @Operation(summary = "Busca un curso",
+            description = "busca un curso por su nombre.")
     public List<Curso> obtenerPorNombre(@RequestParam String nombre) {
         return cursoService.obtenerPorNombre(nombre);
     }
@@ -61,6 +77,8 @@ public class CursoController {
 
 
     @PutMapping("/estado/{id}")
+    @Operation(summary = "Cambiar estado de un curso",
+            description = "Cambia el estado de un curso de activo a inactivo o biciversa.")
     public ResponseEntity<String> cambiarEstado(@PathVariable Integer id) {
     Curso cursoActualizado = cursoService.cambiarEstado(id);
     String mensaje;
