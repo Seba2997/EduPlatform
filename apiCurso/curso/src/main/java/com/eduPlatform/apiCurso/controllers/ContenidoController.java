f93a39408102640dad20d9db94d937913e41d5f7
Fabian9124 <fabi.morales@duocuc.cl>
2025-06-28 01:54:02 -0400

    Hateoas Contenido Assembler y Controller

--- apiCurso/curso/src/main/java/com/eduPlatform/apiCurso/controllers/ContenidoController.java
+++ apiCurso/curso/src/main/java/com/eduPlatform/apiCurso/controllers/ContenidoController.java
@@ -1,18 +1,15 @@
 package com.eduPlatform.apiCurso.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import com.eduPlatform.apiCurso.assemblers.ContenidoModelAssembler;
 import com.eduPlatform.apiCurso.models.entities.Contenido;
 import com.eduPlatform.apiCurso.models.requests.ContenidoCrear;
 import com.eduPlatform.apiCurso.models.requests.ContenidoEditar;
@@ -21,47 +18,61 @@ import com.eduPlatform.apiCurso.services.ContenidoService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 @RestController
 @RequestMapping("/contenidos")
 public class ContenidoController {
-    
+
     @Autowired
     private ContenidoService contenidoService;
 
+    @Autowired
+    private ContenidoModelAssembler contenidoAssembler;
+
     @GetMapping("/")
-    @Operation(summary = "Traer todos los contenidos",
-            description = "Obtiene todos los contenidos cargados en el sistema.")
-    public List<Contenido> traerTodos(){
-        return contenidoService.obtenerTodos();
+    @Operation(summary = "Obtiene todos los contenidos",
+                description = "Devuelve una lista de todos los contenidos registrados.")
+    public CollectionModel<EntityModel<Contenido>> traerTodos() {
+        List<EntityModel<Contenido>> contenidos = contenidoService.obtenerTodos()
+            .stream()
+            .map(contenidoAssembler::toModelMostrarTodos)
+            .collect(Collectors.toList());
+
+        return CollectionModel.of(contenidos,
+            linkTo(methodOn(ContenidoController.class).traerTodos()).withSelfRel());
     }
 
     @GetMapping("/{id}")
-    @Operation(summary = "Traer contenidos por id",
-            description = "Obtiene un contenido por su id correspondient.")
-    public Contenido traerPorId(@PathVariable int id) {
-        return contenidoService.obtenerContenidoPorId(id);
+    @Operation(summary = "Obtiene un contenido por ID",
+                description = "Devuelve un contenido específico según su ID.")
+    public EntityModel<Contenido> traerPorId(@PathVariable int id) {
+        Contenido contenido = contenidoService.obtenerContenidoPorId(id);
+        return contenidoAssembler.toModel(contenido);
     }
 
     @PostMapping("/{idCurso}")
-    @Operation(summary = "Registrar contenido",
-            description = "Registra un nuevo contenido en el sistema.")
-    public Contenido registrarContenido(@Valid @RequestBody ContenidoCrear nuevo, @PathVariable int idCurso) {
-        return contenidoService.registrar(nuevo, idCurso);
+    @Operation(summary = "Crea un nuevo contenido",
+                description = "Crea un nuevo contenido asociado a un curso.")
+    public EntityModel<Contenido> crearContenido(@PathVariable int idCurso, @Valid @RequestBody ContenidoCrear body) {
+        Contenido creado = contenidoService.registrar(body, idCurso);
+        return contenidoAssembler.toModel(creado);
     }
 
     @PutMapping("/{id}")
-    @Operation(summary = "Modificar contenido",
-            description = "Modifica un contenido en el sistema.")
-    public Contenido modificar(@PathVariable Integer id, @Valid @RequestBody ContenidoEditar body) {
-        body.setIdContenido(id); 
-        return contenidoService.modificar(body);
+    @Operation(summary = "Modifica un contenido existente",
+                description = "Permite modificar los datos de un contenido.")
+    public EntityModel<Contenido> modificar(@PathVariable int id, @Valid @RequestBody ContenidoEditar body) {
+        body.setIdContenido(id);
+        Contenido actualizado = contenidoService.modificar(body);
+        return contenidoAssembler.toModelSoloModificar(actualizado);
     }
 
     @DeleteMapping("/{id}")
-    @Operation(summary = "Eliminar contenido",
-            description = "Elimina un contenido en el sistema.")
-    public ResponseEntity<String> eliminarContenido(@PathVariable int id) {
+    @Operation(summary = "Elimina un contenido por ID",
+                description = "Elimina un contenido específico por su ID.")
+    public ResponseEntity<Void> eliminar(@PathVariable int id) {
         contenidoService.eliminarContenido(id);
-        return ResponseEntity.ok("Contenido Eliminado");
+        return ResponseEntity.noContent().build();
     }
 }
