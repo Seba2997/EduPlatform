290d4163854e78415ed36b3f402ed21dbb35ac59
Fabian9124 <fabi.morales@duocuc.cl>
2025-07-05 20:48:39 -0400

    importaciones evaluacion

--- apiCurso/curso/src/main/java/com/eduPlatform/apiCurso/models/entities/Evaluacion.java
+++ apiCurso/curso/src/main/java/com/eduPlatform/apiCurso/models/entities/Evaluacion.java
@@ -1,7 +1,15 @@
 package com.eduPlatform.apiCurso.models.entities;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "evaluacion")
@@ -13,13 +21,13 @@ public class Evaluacion {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
+    
     private String titulo;
-
+    
     private String descripcion;
-
+    
     private int puntajeMaximo;
-
+    
     @ManyToOne
     @JoinColumn(name = "curso_id", nullable = false)
     private Curso curso;
