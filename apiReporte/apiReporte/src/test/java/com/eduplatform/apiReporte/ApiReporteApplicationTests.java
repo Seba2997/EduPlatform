d6b486f1da786e4e39cc181aaf42e1618147400b
Fabian9124 <fabi.morales@duocuc.cl>
2025-07-06 17:48:04 -0400

    apiReporte (vacio)

---
+++ apiReporte/apiReporte/src/test/java/com/eduplatform/apiReporte/ApiReporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduplatform.apiReporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiReporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
