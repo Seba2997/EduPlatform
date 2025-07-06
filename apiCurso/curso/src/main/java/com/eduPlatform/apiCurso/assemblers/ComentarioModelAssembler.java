b71270555cc41d434368fb496e0b82d7c318b787
Fabian9124 <fabi.morales@duocuc.cl>
2025-07-05 22:19:17 -0400

    Hateoas  de Categoria Comentario Evaluacion

---
+++ apiCurso/curso/src/main/java/com/eduPlatform/apiCurso/assemblers/ComentarioModelAssembler.java
@@ -0,0 +1,22 @@
+package com.eduPlatform.apiCurso.assemblers;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import com.eduPlatform.apiCurso.controllers.ComentarioController;
+import com.eduPlatform.apiCurso.models.entities.Comentario;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ComentarioModelAssembler implements RepresentationModelAssembler<Comentario, EntityModel<Comentario>> {
+
+    @Override
+    public @NonNull EntityModel<Comentario> toModel(@NonNull Comentario comentario) {
+        return EntityModel.of(comentario,
+            linkTo(methodOn(ComentarioController.class).listarPorCurso(comentario.getCurso().getId())).withRel("comentarios-del-curso"),
+            linkTo(methodOn(ComentarioController.class).editar(null)).withRel("modificar-comentario")
+        );
+    }
+}
