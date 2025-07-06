d5c4d8fb9ad063017c41ef19ed86eb1645dcfd73
Seba2997 <valdebenito732@gmail.com>
2025-07-06 02:11:00 -0400

    Corregido la creacion de Tickets

--- apiSoporte/apiSoporte/src/main/java/com/eduplatform/apiSoporte/models/request/TicketCrear.java
+++ apiSoporte/apiSoporte/src/main/java/com/eduplatform/apiSoporte/models/request/TicketCrear.java
@@ -1,5 +1,6 @@
 package com.eduplatform.apiSoporte.models.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TicketCrear {
+
+    @NotBlank(message = "El asunto no puede estar vacío")
     private String asunto;
+    
+    @NotBlank(message = "La descripción no puede estar vacía")
     private String descripcion;
 }
