cdba24f7e908d111375cc5c13126037ed2d3e452
Fabian9124 <fabi.morales@duocuc.cl>
2025-07-07 23:45:13 -0400

    constructores boleta

--- apiReporte/apiReporte/src/main/java/com/eduplatform/apiReporte/models/external/Boleta.java
+++ apiReporte/apiReporte/src/main/java/com/eduplatform/apiReporte/models/external/Boleta.java
@@ -1,10 +1,14 @@
 package com.eduplatform.apiReporte.models.external;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Boleta {
      private int id;
     private int numeroBoleta;
