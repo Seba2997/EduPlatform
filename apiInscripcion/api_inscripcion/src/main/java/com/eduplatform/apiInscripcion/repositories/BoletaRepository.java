1b44d06b3348b689c986a46a41243de5019a74a6
Seba2997 <valdebenito732@gmail.com>
2025-07-07 23:43:52 -0400

    Agregados controladores para en apiInscripcion para permitir que el admin pueda obtener los datos en apiReportes

--- apiInscripcion/api_inscripcion/src/main/java/com/eduplatform/apiInscripcion/repositories/BoletaRepository.java
+++ apiInscripcion/api_inscripcion/src/main/java/com/eduplatform/apiInscripcion/repositories/BoletaRepository.java
@@ -1,5 +1,7 @@
 package com.eduplatform.apiInscripcion.repositories;
 
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.eduplatform.apiInscripcion.models.entities.Boleta;
@@ -8,5 +10,7 @@ public interface BoletaRepository extends JpaRepository<Boleta, Integer> {
     
 
     Boolean existsByNumeroBoleta(int numeroBoleta);
+
+    Boleta findByInscripcionId(int inscripcionId);
     
 }
